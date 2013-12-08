d8b9690171c6966a98d69c5b85fb96fa7ec123d3
Martin Vium <martin.vium@gmail.com>
2013-12-08 10:52:57 +0100

    Remove component name in hidden inspector functionality

--- lib/src/editor/field_drawer.dart
+++ lib/src/editor/field_drawer.dart
@@ -11,7 +11,6 @@ class FieldDrawer {
     var badge = new SpanElement()
       ..classes.add('badge pointer')
       ..text = value.toString()
-      ..dataset["component"] = _component.name
       ..dataset["field"] = name.toString()
       ..onClick.listen(onClickBadge);
     
@@ -24,18 +23,18 @@ class FieldDrawer {
   
   onClickBadge(Event e) {
     Element badge = e.target;
-    var drawer = _createDrawer(badge.dataset["component"], badge.dataset["field"]);
+    var drawer = _createDrawer(badge.dataset["field"]);
     badge.parent.append(drawer);
   }
   
-  _createDrawer(String componentName, String fieldName) {
-    var field = _fieldByName(componentName, fieldName);
+  _createDrawer(String fieldName) {
+    var field = _fieldByName(fieldName);
     var value = field.reflectee.toString();
     
     var form = new FormElement()
       ..onSubmit.listen(_saveField);
     
-    var meta = new Element.html('<div><input type="hidden" name="component" value="${componentName}"/><input type="hidden" name="field" value="${fieldName}"/></div>');
+    var meta = new Element.html('<div><input type="hidden" name="field" value="${fieldName}"/></div>');
     var drawer = new Element.html('<div class="form-group"><input type="text" name="value" value="${value}" class="form-control"/></div>');
     var button = new Element.html('<div class="form-group"><input type="submit" value="Save" class="btn btn-default"/></div>');
     
@@ -48,23 +47,22 @@ class FieldDrawer {
   _saveField(Event e) {
     FormElement form = e.target;
     
-    var componentElement = form.querySelector('input[name="component"]') as HiddenInputElement;
     var fieldElement = form.querySelector('input[name="field"]') as HiddenInputElement;
-    var field = _fieldByName(componentElement.value, fieldElement.value);
+    var field = _fieldByName(fieldElement.value);
     var input = form.querySelector('input[name="value"]') as TextInputElement;
     
-    _setField(componentElement.value, fieldElement.value, input.value);
+    _setField(fieldElement.value, input.value);
     
     e.preventDefault();
   }
   
-  InstanceMirror _fieldByName(String componentName, String fieldName) {
+  InstanceMirror _fieldByName(String fieldName) {
     var fieldSym = new Symbol(fieldName);
     var field = reflect(_component).getField(fieldSym);
     return field;
   }
   
-  _setField(String componentName, String fieldName, String value) {
+  _setField(String fieldName, String value) {
     var fieldSym = new Symbol(fieldName);
     var mirror = reflect(_component);
     var field = mirror.getField(fieldSym);
